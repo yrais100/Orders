628c48b20bdce456b1c7da44546206525111b5b6
yrais100 <yairarboleda117427@correo.itm.edu.co>
2024-03-15 08:31:59 -0500

    First page in razor, Countries.

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -7,6 +7,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7191/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7220/") });
 builder.Services.AddScoped<IRepository, Repository>();
 await builder.Build().RunAsync();
