f33551886957de3a212e87560b0c1e607c5aa975
yrais100 <yairarboleda117427@correo.itm.edu.co>
2024-03-18 08:25:31 -0500

    Create generic class.

---
+++ Orders/Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] IRepository Repository { get; set; } = null!;
+        public List<Country>? Countries { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+            Countries = responseHttp.Response;
+        }
+    }
+}
