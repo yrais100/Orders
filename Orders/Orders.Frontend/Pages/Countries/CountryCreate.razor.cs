cee9b184908f34e8e7422675d610a49ca39191a3
yrais100 <yairarboleda117427@correo.itm.edu.co>
2024-03-21 08:58:41 -0500

    Create Form "Pais"

---
+++ Orders/Orders.Frontend/Pages/Countries/CountryCreate.razor.cs
@@ -0,0 +1,43 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountryCreate
+    {
+        private Countryform? countryform;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        private Country country = new();
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await Repository.PostAsync("/api/countries", country);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsyn();
+                await SweetAlertService.FireAsync("Error", message);
+                return;
+            }
+
+            Return();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+        private void Return()
+        {
+            countryform!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("/countries");
+        }
+
+    }
+}
---
+++ Orders/Orders.Frontend/Pages/Countries/Countryform.razor.cs
@@ -0,0 +1,51 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components;
+using Orders.Shared.Entities;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class Countryform
+    {
+        private EditContext editContext = null!;
+
+        protected override void OnInitialized()
+        {
+            editContext = new(Country);
+        }
+
+        [EditorRequired, Parameter] public Country Country { get; set; } = null!;
+        [EditorRequired, Parameter] public EventCallback OnValidSubmit { get; set; }
+        [EditorRequired, Parameter] public EventCallback ReturnAction { get; set; }
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        public bool FormPostedSuccessfully { get; set; } = false;
+
+        private async Task OnBeforeInternalNavigation(LocationChangingContext context)
+        {
+            var formWasEdited = editContext.IsModified();
+
+            if (!formWasEdited || FormPostedSuccessfully)
+            {
+                return;
+            }
+
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmación",
+                Text = "¿Deseas abandonar la página y perder los cambios?",
+                Icon = SweetAlertIcon.Warning,
+                ShowCancelButton = true
+            });
+
+            var confirm = !string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+
+            context.PreventNavigation();
+        }
+
+    }
+}
